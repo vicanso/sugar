8fa9d1f5f6a4ee312584118ea2b20ea8619e414d
vicanso <vicansocanbico@gmail.com>
2015-10-13 20:29:34 +0800

    commit

--- examples/app.js
+++ examples/app.js
@@ -1,15 +1,16 @@
 'use strict';
-const html = require('./lib/html');
+const html = require('../lib/html');
 const co = require('co');
 const fs = require('fs');
+const JsDiff = require('diff');
 const _ = require('lodash');
 
 co(function* () {
-  let testHtml = yield html.get('http://test.gf.com.cn/portal/index');
-  let t1Html = yield html.get('http://t1.gf.com.cn/portal/index');
-  // fs.writeFile('./test.html', html.format(testHtml));
-  // fs.writeFile('./t1.html', html.format(t1Html));
-  let result = html.getDiffView(testHtml, t1Html);
+  let originalHtml = fs.readFileSync(
+    './original.html', 'utf8');
+  let currentHtml = fs.readFileSync('./index.html',
+    'utf8');
+  let result = html.getDiffView(currentHtml, originalHtml);
   fs.writeFile('./diff.html',
     '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style>.jtDiffAdd{color:green}.jtDiffRemove{color:red}</style></head><body><pre>' +
     result +
